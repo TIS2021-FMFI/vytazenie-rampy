919f45d0d9c3972943365de6b30d3b008f42ece2
Filip Lajčin <termoxinprod@gmail.com>
2021-12-04 12:05:27 +0100

    Fixnute vytvaranie pouzivatela + prelozene texty v admine + username sa vyplna automaticky pri vypisovani mena a priezviska.

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -3,19 +3,39 @@ from django.contrib.auth.forms import (
     ReadOnlyPasswordHashField,
     UserCreationForm,
     UserChangeForm,
-    UsernameField,
+    UsernameField
 )
 from .models import CustomUser
 
 from django.utils.translation import gettext_lazy as _
 
-
+help_text = '''
+<ul>
+    <li>Vaše heslo nemôže byť príliš podobné ostatným údajom, ktoré ste vo formulári vyplnili.</li>
+    <li>Vaše heslo musí obsahovať aspoň 8 znakov.</li>
+    <li>Vaše heslo nemôže byť bežne používané heslo.</li>
+    <li>Vaše heslo nemôže obsahovať iba číslice.</li>
+</ul>
+'''
 class CustomUserCreationForm(UserCreationForm):
+    disabled_fields = ['username']
+
+    password1 = forms.CharField(
+        label=_("Password"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
+        help_text=help_text,
+    )
     class Meta:
         model = CustomUser
         fields = ("username", "email")
-        field_classes = {"username": UsernameField}
+        field_classes = {"username": UsernameField, "email": forms.EmailField}
 
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+
+        for field in self.disabled_fields:
+            self.fields[field].widget.attrs['readonly'] = True
 
 class CustomUserChangeForm(UserChangeForm):
     password = ReadOnlyPasswordHashField(
